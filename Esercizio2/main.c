f6992f892383797a7dea808af3ad2dac0a7dd005
nicolovinci <12999303@itisrossi.vi.it>
2026-01-14 10:57:36 +0100

    Done Exercise2

--- Esercizio2/main.c
+++ Esercizio2/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+int function(int secondiInput, int* ore, int* minuti, int* secondi)
+{
+    *ore = secondiInput / 3600;
+    *minuti = secondiInput / 60;
+    *secondi = secondiInput;
+    return 0;
+}
+
 int main(void)
 {
-    printf("Hello, World!\n");
+    int secondiInput = 3600, ore, minuti, secondi;
+    function(secondiInput, &ore, &minuti, &secondi);
+    printf("%d secondi sono %d ore, %d minuti o %d secondi\n", secondiInput, ore, minuti, secondi);
     return 0;
 }
