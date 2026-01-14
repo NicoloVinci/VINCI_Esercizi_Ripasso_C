9c3afd420cc9a8c1b7de6ec9dd63d3e01a312a58
nicolovinci <12999303@itisrossi.vi.it>
2026-01-14 11:16:48 +0100

    Done Exercise3

--- Esercizio3/main.c
+++ Esercizio3/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+int potenza(int base, int esponente)
+{
+    if (esponente == 0)
+        return 1;
+    int risultato = 1;
+    for (int i = 0; i < esponente; i++)
+        risultato *= base;
+    return risultato;
+}
+
 int main(void)
 {
-    printf("Hello, World!\n");
+    int a = 3, b = 4, risultato;
+    risultato = potenza(a, b);
+    printf("%d^%d = %d\n", a, b, risultato);
+    risultato = potenza(b, a);
+    printf("%d^%d = %d\n", b, a, risultato);
     return 0;
 }
