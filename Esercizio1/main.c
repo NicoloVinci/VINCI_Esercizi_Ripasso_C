ab30e808fe734bc3b4f54d2181638a7ff290b26c
nicolovinci <12999303@itisrossi.vi.it>
2026-01-14 10:22:52 +0100

    Done

--- Esercizio1/main.c
+++ Esercizio1/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int divisione(int a, int b, int *q, int *r)
+{
+    if (b == 0)
+    {
+        return -1;
+    }
+    *q = a / b;
+    *r = a % b;
+    return 0;
+}
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    int a = 3, b = 2;
+    int *quoziente = malloc(sizeof(int)), *resto = malloc(sizeof(int));
+    int ris = divisione(a, b, quoziente, resto);
+    if (ris == -1)
+    {
+        printf("Errore: divisione per zero.\n");
+    }
+    else
+    {
+        printf("Quoziente: %d, Resto: %d\n", *quoziente, *resto);
+    }
     return 0;
 }
